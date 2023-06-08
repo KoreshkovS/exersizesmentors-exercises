450729ae0c92352de4f2436cb5bd1f21bd7e5918
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-06-08 17:34:30 +0300

    6 task

--- js/6.js
+++ js/6.js
@@ -2,16 +2,22 @@
 // fn([1, 2, '2', true, null, 2, 2, false, true, 'null])
 // [1, 2, '2', true, null, false, 'null]
 
-const arry = [1, 2, "2", true, null, 2, 2, false, true, "null"];
+const array = [1, 2, "2", true, null, 2, 2, false, true, "null"];
 
-const deleteDubs = function (arry) {
-  let newArry = [];
-  for (let i = 0; i < arry.length; i++) {
-    if (arry[i] !== arry[i + 1]) {
-      newArry.push(arry[i]);
-    }
-  }
-  return newArry;
-};
+// const deleteDubs = function (array) {
+//   let newArray = [];
+//   for (let i = 0; i < array.length; i++) {
+//     if (array[i] !== array[i + 1]) {
+//       newArray.push(array[i]);
+//     }
+//   }
+//   return newArray;
+// };
 
-console.log(deleteDubs(arry));
+// console.log(deleteDubs(array));
+
+const uniqueArray = array.filter(function (value, index, self) {
+  return self.indexOf(value) === index;
+});
+
+console.log(uniqueArray);
