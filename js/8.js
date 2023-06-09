79378a77665cb32a8c92a6397117fb28572ddfb9
KoreshkovS <110051047+KoreshkovS@users.noreply.github.com>
2023-06-09 09:16:13 +0300

    8 task

--- js/8.js
+++ js/8.js
@@ -2,13 +2,27 @@
 // fn(5)
 // [1, 1, 2, 3, 5]
 
-const fibanachi = [
-  0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987,
-];
+// F(0) = 0,
+// F(1) = 1,
+// F(2) = F(1) + F(0) = 1 + 0 = 1,
+// F(3) = F(2) + F(1) = 1 + 1 = 2,
+// F(4) = F(3) + F(2) = 2 + 1 = 3,
+// F(5) = F(4) + F(3) = 3 + 2 = 5,
 
 const x = 5;
 
-const spliseX = fibanachi.splice(0, x);
+const fibanachiRow = function (x) {
+  const newArray = [1];
+  let sum = 0;
+  let f0 = 0;
+  let f1 = 1;
+  for (let i = 0; i < x - 1; i++) {
+    sum = f1 + f0;
+    f0 = f1;
+    f1 = sum;
+    newArray.push(sum);
+  }
+  return newArray;
+};
 
-console.log(spliseX);
-
+console.log(fibanachiRow(x));
